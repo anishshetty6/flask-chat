39d6a2e41ce2d7cba9885ff6d59359a4a0edcd4e
anishshetyy6 <anishshetty2708@gmail.com>
2024-05-01 01:49:52 +0530

    functionalities done

--- main.py
+++ main.py
@@ -51,8 +51,54 @@ def home():
 
 @app.route("/room")
 def room():
-    return render_template("room.html")
+    room=session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))          #so that directly one cant access /room
 
+    return render_template("room.html",code=room, messages=rooms[room]["messages"])
+
+@socketio.on("message")            #same event which is emitted in last func in room.html is restransmitted to other users in the room
+def message(data):
+    room=session.get("room")
+    if room not in rooms:
+        return
+    
+    content={
+        "name":session.get("name"),
+        "message":data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)         #to store messages in room
+    print(f"{session.get('name')} said: {data['data']}")
+
+@socketio.on("connect")
+def connect(auth):
+    room=session.get("room")
+    name=session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name":name,"message":"has joined the room"},to=room)
+    rooms[room]["members"]+=1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room=session.get("room")
+    name=session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"]-=1
+        if rooms[room]["members"]<=0:
+            del rooms[room]
+
+    send({"name":name, "message":"has left the room"},to=room)
+    print(f"{name} has left the room {room}")
 
 if __name__=="__main__":
     socketio.run(app,debug=True)
